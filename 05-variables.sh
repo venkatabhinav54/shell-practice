de56a571360a6ccec991a2ee5625c5072ef12975
venkatabhinav54 <venkatabhinav54@gmail.com>
2024-08-26 15:06:35 +0530

    adding variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,12 +2,12 @@
 
 echo "Please enter your username::"
 
-read -s USERNAME
+read -s USERNAME #it takes input into USERNAME variable
 
 echo "Username entered is: $USERNAME"
 
 echo "Please entered password::"
 
-read -s PASSWORD
+read -s PASSWORD  #it takes input into PASSWORD variable
 
 echo "Password entered is: $PASSWORD"
---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+FRUITS=("APPLE" "BANANA" "ORANGE" "KIWI")
+
+echo "Frist fruit is: ${FRUITS[0]}"
+
+echo "Second fruit is: ${FRUITS[1]}"
+
+echo "Third fruit is: ${FRUITS[2]}"
+
+echo "All Fruits is: $@
