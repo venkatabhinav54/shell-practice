d20b66221750755b3011da4a21f9d65f6d498af5
venkatabhinav54 <venkatabhinav54@gmail.com>
2024-08-26 14:46:34 +0530

    variables program

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Mahesh
 PERSON2=Prakash
 
-echo "PERSON1:: Hi PERSON2, How Are you?"
-echo "PERSON2:: Hello PERSON1. I am fine. How are you doing?"
-echo "RPERSON1:: Yeah I am doing good PERSON2, What's going on."
-echo "PERSON2:: I started learnin Shell Script PERSON1"
+echo "$PERSON1:: Hi $PERSON2, How Are you?"
+echo "$PERSON2:: Hello $PERSON1. I am fine. How are you doing?"
+echo "$PERSON1:: Yeah I am doing good ${PERSON2}, What's going on."
+echo "$PERSON2:: I started learnin Shell Script ${PERSON1}"
