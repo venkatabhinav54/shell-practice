2fea0488a1b280e7670b510ff285715c56c499c1
venkatabhinav54 <venkatabhinav54@gmail.com>
2024-08-26 13:28:29 +0530

    I adding conversation file

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How Are you?"
+echo "Suresh:: Hello Ramesh. I am fine. How are you doing?"
+echo "Ramesh:: Yeah I am doing good Suresh, What's going on."
+echo "Suresh:: I started learnin Shell Script Ramesh"
