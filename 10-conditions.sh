86e9eee70336425318175a0f9a3a6f38469b2dd5
venkatabhinav54 <venkatabhinav54@gmail.com>
2024-08-28 12:11:41 +0530

    Shell

---
+++ 10-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER1=$1
+
+if [ $NUMBER1 -gt 20 ]
+then 
+    echo "Given Number: $NUMBER1 is greater than 20"
+else 
+    echo "Given Number: $NUMBER1 is lessthan 20"
+fi         
