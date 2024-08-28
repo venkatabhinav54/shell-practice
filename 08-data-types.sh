1cf9ef50cb0ecbe32ae3af06c913611d6d56f147
venkatabhinav54 <venkatabhinav54@gmail.com>
2024-08-28 11:51:16 +0530

    Shell

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -5,7 +5,10 @@ NUMBER2=$2
 
 SUM=$(($NUMBER1+$NUMBER2))
 MUL=$(($NUMBER1*$NUMBER2))
+DEV=$(($NUMBER1/$NUMBER2))
 
 echo "Total of $NUMBER1 and $NUMBER2 is: $SUM"
 
 echo "Multiple of $NUMBER1 and $NUMBER2 is: $MUL"
+
+echo "Devide by $NUMBER! and $NUMBER2 is: $DEV"
