a46ba01df0f936468e7dec04b1a230df8e2f972b
venkatabhinav54 <venkatabhinav54@gmail.com>
2024-08-28 11:48:41 +0530

    Shell

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -4,5 +4,8 @@ NUMBER1=$1
 NUMBER2=$2
 
 SUM=$(($NUMBER1+$NUMBER2))
+MUL=$(($NUMBER1*$NUMBER2))
 
 echo "Total of $NUMBER1 and $NUMBER2 is: $SUM"
+
+ECHO "Multiple of $NUMBER1 and $NUMBER2 is: $MUL"
