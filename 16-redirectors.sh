f6cef4fe50cee6e70788594608fd1cb354b477f3
venkatabhinav54 <venkatabhinav54@gmail.com>
2024-08-28 22:31:07 +0530

    Shell script

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -2,7 +2,7 @@
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(DATE +%Y-%m-%d-%H-%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER 
 
