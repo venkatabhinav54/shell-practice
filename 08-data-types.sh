342577bf20b3b209e3c291ebdc7b351b4a2ae26a
venkatabhinav54 <venkatabhinav54@gmail.com>
2024-12-16 19:53:14 +0530

    data-types

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# NUMBER1=$1
+# NUMBER2=$2
+
+# SUM=$(($NUMBER1+$NUMBER2))
+# MUL=$(($NUMBER1*$NUMBER2))
+# DEV=$(($NUMBER1/$NUMBER2))
+
+# echo "Total of $NUMBER1 and $NUMBER2 is: $SUM"
+
+# echo "Multiple of $NUMBER1 and $NUMBER2 is: $MUL"
+
+# echo "Devide by $NUMBER1 and $NUMBER2 is: $DEV"
+
+
 NUMBER1=$1
 NUMBER2=$2
 
@@ -7,8 +21,6 @@ SUM=$(($NUMBER1+$NUMBER2))
 MUL=$(($NUMBER1*$NUMBER2))
 DEV=$(($NUMBER1/$NUMBER2))
 
-echo "Total of $NUMBER1 and $NUMBER2 is: $SUM"
-
-echo "Multiple of $NUMBER1 and $NUMBER2 is: $MUL"
-
-echo "Devide by $NUMBER1 and $NUMBER2 is: $DEV"
+echo "total of $NUMBER1 and $NUMBER2 is: $SUM"
+echo "multiple of $NUMBER! and $NUMBER2 is: $MUL"
+echo "devision os $NUMBER1 and $NUMBER2 is: $DEV"
